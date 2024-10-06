8504c9c0ef2c573971cfe0d3a01a5b56926fa7de
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-06 06:25:23 +0000

    feat: add ability to update accounts

---
+++ middlewares/auth/updateAccount.ts
@@ -0,0 +1,154 @@
+import type { RouterContext } from "jsr:@oak/oak/router";
+import { decodeBase64, encodeBase64 } from "jsr:@std/encoding";
+import type User from "types/user";
+import {
+	ALGO,
+	cryptoKey,
+	SALT_ALGO,
+	saltCryptoKey,
+} from "authentication/crypto";
+import db from "database";
+
+const updateAccount = async (
+	ctx: RouterContext<
+		"/update",
+		Record<string | number, string | undefined>,
+		// deno-lint-ignore no-explicit-any
+		Record<string, any>
+	>,
+) => {
+	const { username: oldUsername, newUsername, passphrase }: {
+		username: string;
+		newUsername: string;
+		passphrase: string;
+	} = await ctx.request.body.json();
+	if (!newUsername && !passphrase) {
+		ctx.response.status = 422;
+		ctx.response.body = {
+			message: "Username or passphrase field cannot be empty",
+			status: 422,
+		};
+
+		return;
+	}
+
+	const stmt = db.prepare(`
+		SELECT username, passphrase, salt FROM users WHERE username = '${newUsername}';
+	`);
+
+	const stmtUpdate = db.prepare(`
+		SELECT username, passphrase, salt FROM users WHERE username = '${oldUsername}';
+	`);
+
+	const userExists = stmt.get<User>() !== undefined;
+	const user = stmtUpdate.get<User>();
+
+	if (!user) {
+		ctx.response.status = 503;
+		ctx.response.body = {
+			message: `Not able to retrieve \`${oldUsername}\``,
+			status: 503,
+		};
+
+		return;
+	}
+
+	if (!userExists) {
+		let usernameChangeMessage = "";
+		let passphraseChangeMessage = "";
+		const randSalt = crypto.getRandomValues(new Uint8Array(16));
+		const enc = new TextEncoder();
+		const dec = new TextDecoder();
+		const decodedEncryptedPassphrase = decodeBase64(user.passphrase);
+		const decodedEncryptedSalt = decodeBase64(user.salt);
+		const decryptPassphrase = await crypto.subtle.decrypt(
+			ALGO,
+			cryptoKey,
+			decodedEncryptedPassphrase,
+		);
+
+		const decryptSalt = await crypto.subtle.decrypt(
+			SALT_ALGO,
+			saltCryptoKey,
+			decodedEncryptedSalt,
+		);
+
+		const decryptPassphraseString = dec.decode(decryptPassphrase);
+		const salt = dec.decode(decryptSalt);
+		const saltedPassphrase = passphrase.concat(salt);
+
+		if (newUsername === oldUsername) {
+			usernameChangeMessage += "Username unchanged";
+		} else {
+			usernameChangeMessage +=
+				`Username changed from \`${oldUsername}\` to \`${newUsername}\``;
+		}
+
+		if (decryptPassphraseString === saltedPassphrase) {
+			passphraseChangeMessage += "Passphrase unchanged";
+		} else {
+			passphraseChangeMessage += "Passphrase changed";
+		}
+
+		if (
+			(newUsername === oldUsername) &&
+			(decryptPassphraseString === saltedPassphrase)
+		) {
+			ctx.response.status = 304;
+			ctx.response.body = {
+				message:
+					`No changes were being sent. ${usernameChangeMessage}. ${passphraseChangeMessage}`,
+				status: 304,
+			};
+			return;
+		}
+
+		const newSalt = encodeBase64(randSalt);
+		const saltPayload = enc.encode(newSalt);
+		const passphrasePayload = enc.encode(passphrase.concat(newSalt));
+		const encryptPassphrase = await crypto.subtle.encrypt(
+			ALGO,
+			cryptoKey,
+			passphrasePayload,
+		);
+		const encryptSalt = await crypto.subtle.encrypt(
+			SALT_ALGO,
+			saltCryptoKey,
+			saltPayload,
+		);
+
+		const encryptedPassphrase = encodeBase64(encryptPassphrase);
+		const encryptedSalt = encodeBase64(encryptSalt);
+		const changes = db.exec(
+			`
+			UPDATE users 
+			SET username = '${newUsername}', 
+				passphrase = '${encryptedPassphrase}',
+				salt = '${encryptedSalt}'
+			WHERE
+				username = '${oldUsername}'
+			`,
+		);
+		console.log(changes);
+		console.log(`
+			username: ${newUsername},
+			passphrase: ${passphrase}`);
+		// Handle it here
+		ctx.response.body = {
+			message: `${usernameChangeMessage}. ${passphraseChangeMessage}`,
+			status: 200,
+		};
+
+		return;
+	}
+
+	ctx.response.status = 409;
+	ctx.response.body = {
+		message: `Username \`${newUsername}\` already taken`,
+		status: 409,
+	};
+
+	return;
+};
+
+export default updateAccount;
