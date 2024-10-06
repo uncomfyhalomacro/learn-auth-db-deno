04bea6a491a205867aee563d28ecf084d69d70ce
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-06 00:27:11 +0000

    split up logic to multiple independent modules for imports

---
+++ authentication/crypto.ts
@@ -0,0 +1,41 @@
+import { crypto } from "jsr:@std/crypto";
+import { decodeBase64 } from "@std/encoding";
+
+const TOKEN = decodeBase64(Deno.env.get("API_KEY") ?? "");
+const SALT_TOKEN = decodeBase64(Deno.env.get("SALT_SECRET") ?? "");
+
+export const ALGO = {
+	name: "AES-GCM",
+	// also our nonce. it can only be used ONCE. So if server dies
+	// then database dies.
+	// FIXED: WORKAROUND IS TO USE `importKey`
+	iv: TOKEN,
+};
+
+export const SALT_ALGO = {
+	name: "AES-GCM",
+	// also our nonce. it can only be used ONCE. So if server dies
+	// then database dies.
+	// FIXED: WORKAROUND IS TO USE `importKey`
+	iv: SALT_TOKEN,
+};
+
+export const cryptoKey = await crypto.subtle.importKey(
+	"raw",
+	TOKEN,
+	{
+		name: "AES-GCM",
+	},
+	true,
+	["encrypt", "decrypt"],
+);
+
+export const saltCryptoKey = await crypto.subtle.importKey(
+	"raw",
+	SALT_TOKEN,
+	{
+		name: "AES-GCM",
+	},
+	true,
+	["encrypt", "decrypt"],
+);
---
+++ types/user.ts
@@ -0,0 +1,6 @@
+export default interface User {
+	id: number;
+	username: string;
+	passphrase: string;
+	salt: string;
+}
