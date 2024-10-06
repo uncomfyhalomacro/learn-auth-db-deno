2d469dfcd2840fcaeadefc6807b5b0115f22f01d
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-06 06:24:54 +0000

    auth: block routes by checking if user is authenticated with a new auth handler

--- main.ts
+++ main.ts
@@ -3,18 +3,35 @@ import "jsr:@std/dotenv/load";
 import register from "middlewares/register";
 import login from "middlewares/login";
 import vip from "middlewares/vip";
+import checkAuth from "authentication/checkAuth";
+import updateAccount from "middlewares/auth/updateAccount";
 
 const router = new Router();
+const authRouter = new Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
 
+// This will be used for comparing with `/hello` which is an auth-checked route
+// The reason why is because we can check authentication status
+// to authenticated/authorised needed actions per route, hence, why
+// we have a separate `Router` instance called `authRouter`.
 router.get("/vip", vip);
+
+authRouter.get("/hello", (ctx) => {
+  ctx.response.body = "Hello world";
+});
+
+authRouter.put("/update", updateAccount);
 
 const app = new Application();
 
+// No auth routes
 app.use(router.routes());
+
+// Auth Routes
+app.use(checkAuth, authRouter.routes());
 app.use(router.allowedMethods({
   throw: false,
 }));
